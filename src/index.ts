19e77d6af6990196f4463bca61712bbfdf6d198c
ryuring <egashira@catchup.co.jp>
2025-07-17 13:07:05 +0900

    不要なコメントを削除

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ async function handleRequest(input: any) {
         {
           name: 'getArticles',
           description: 'ブログ記事一覧を取得します。',
-          params: []へるぷ　
+          params: []
         },
         {
           name: 'health',
